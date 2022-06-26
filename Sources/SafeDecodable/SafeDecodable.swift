3b50fcaef6029a14bb4fce741980a9ff3a592e13
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-06-26 21:13:07 +0300

    Implement SafeDecodable

--- Sources/SafeDecodable/SafeDecodable.swift
+++ Sources/SafeDecodable/SafeDecodable.swift
@@ -1,1 +1,23 @@
+@propertyWrapper
+public struct SafeDecodable<T: Decodable>: Decodable {
+    public let wrappedValue: T?
 
+    public init(wrappedValue: T?) {
+        self.wrappedValue = wrappedValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.singleValueContainer()
+            wrappedValue = try (container.decode(OptionalDecodable<T>.self)).value
+        } catch {
+            wrappedValue = nil
+        }
+    }
+}
+
+public extension KeyedDecodingContainer {
+    func decode<T>(_ type: SafeDecodable<T>.Type, forKey key: Self.Key) throws -> SafeDecodable<T> where T: Decodable {
+        try decodeIfPresent(type, forKey: key) ?? SafeDecodable<T>(wrappedValue: nil)
+    }
+}
